91fec6e0a47a14596e692769d73a9ba0610bccc0
raisali <raisali_1@hotmail.com>
2022-11-22 04:20:15 -0500

    2 files modified

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 import './style.css';
 
 //Gráfico de columnas. Mi 1o gráfico
+//Gráfico de columnas. Mi 1º gráfico
+var visualization = d3plus.viz()
+ .container("#viz1")
+ .data([{"year": 1991, "name":"alpha", "value": 15},
+ {"year": 1992, "name":"alpha", "value": 20},])
+ .type("bar")
+ .id("name")
+ .x("year")
+ .y("value")
+ .draw()
